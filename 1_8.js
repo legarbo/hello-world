31dd3cdfc8a5f8edb195c3e5e1eb9ae23b6c9bfa
Leopoldo Garcia <legarbo@gmail.com>
2016-10-01 21:10:37 -0400

    added 1_8.js

---
+++ 1_8.js
@@ -0,0 +1,28 @@
+module.exports = Strings_1_8 = (function() {
+  var isSubstring = function(str1, str2) { // check if str2 is a substring of str1
+    return str1.indexOf(str2) != -1;
+  };
+
+  var sameLengthAndNotBlank = function(str1, str2) { // check if both strings have same length
+   // and str1 is not empty
+    var len = str1.length;
+    return len === str2.length && len > 0;
+  };
+
+  return {
+    isRotation: function(str1, str2) {
+      if(!sameLengthAndNotBlank(str1, str2)) {
+        return false;
+      }
+      if(isSubstring(str1+str1, str2)) {
+        return true;
+      }
+    }
+  };
+}());
+EXTRA
+EXTRA
+EXTRA
+EXTRA
+EXTRA
+EXTRA
